a2fd03004ab12ceddc649c936ad6f057a92a8395
Natalie Akam <natalie.akam@gmail.com>
2015-09-10 11:53:44 +0100

    Added product info

--- test/e2e/clothingShopFeature.js
+++ test/e2e/clothingShopFeature.js
@@ -1,5 +1,7 @@
 describe('Clothing Shop Homepage', function() {
 
+  var productList = element.all(by.repeater('product in products'));
+
   beforeEach(function() {
     browser.get('http://localhost:8080');
   });
@@ -9,8 +11,13 @@ describe('Clothing Shop Homepage', function() {
   });
 
   it('displays products', function() {
-    var productList = element.all(by.repeater('product in products'));
     expect(productList.count()).toEqual(13);
-    expect(productList.get(0).getText()).toEqual('Almond Toe Court Shoes, Patent Black');
+  });
+
+  it('displays product details', function() {
+    expect(productList.get(0).element(by.css('.product-name')).getText()).toEqual('Almond Toe Court Shoes, Patent Black');
+    expect(productList.get(2).element(by.css('.product-category')).getText()).toEqual('Men\'s Footwear');
+    expect(productList.get(6).element(by.css('.product-price')).getText()).toEqual('£30.00');
+    expect(productList.get(12).element(by.css('.product-stock')).getText()).toEqual('5 in stock');
   });
 });
